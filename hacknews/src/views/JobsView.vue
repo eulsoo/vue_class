20d28239c40b314eda6ba1a227c1a0e9a0bdb2f6
eulsoo <eulsoo.jung@gmail.com>
2020-02-24 12:02:13 +0900

    2) axios로 데이터 가져오기

--- hacknews/src/views/JobsView.vue
+++ hacknews/src/views/JobsView.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>Jobs view</div>
+  <ol class="list">
+    <li v-for="item in list" v-bind:key="item.id">
+      <b>{{ item.time_ago}}</b>
+      <span> by {{ item.user }}</span><br/>
+      <a v-bind:href="item.url">{{ item.title }}</a>
+    </li>
+  </ol>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  data() {
+    return {
+      list:[]
+    }
+  },
+  created() {
+    axios
+      .get(`https://api.hnpwa.com/v0/jobs/1.json`)
+      .then( res => {this.list = res.data})
+  }
 }
 </script>
 
--- hacknews/src/views/NewsView.vue
+++ hacknews/src/views/NewsView.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>News view</div>
+  <ol class="list">
+    <li v-for="item in list" v-bind:key="item.id">
+      <b>{{ item.time_ago}}</b>
+      <span> by {{ item.user }}</span><br/>
+      <a v-bind:href="item.url">{{ item.title }}<i>({{item.comments_count}})</i></a>
+    </li>
+  </ol>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  data() {
+    return {
+      list:[]
+    }
+  },
+  created() {
+    axios
+      .get(`https://api.hnpwa.com/v0/news/1.json`)
+      .then( res => {this.list = res.data})
+  }
 }
 </script>
 
 <style>
-
+.list li {padding-left:10px; list-style-type: none}
+.list li + li {margin-top:20px}
+.list li > a {text-decoration: none; font-size:22px}
+.list li > b {font-style: normal; font-size:12px; color:#999}
+.list li span {font-style: normal; font-weight:700; font-size:12px; color:#999;}
+.list li i {font-style: normal; font-size:12px; color:cornflowerblue}
 </style>
